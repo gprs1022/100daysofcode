00293fc75f3b1b7cc7f0f5a69f83dd607a79871b
Pradeep Singh <88311782+gprs1022@users.noreply.github.com>
2023-08-17 21:53:55 +0530

    Update pattern2.cpp

--- Day6-12/pattern2.cpp
+++ Day6-12/pattern2.cpp
@@ -52,8 +52,8 @@ void print5(int n){
         cout<<endl;
     }
 }
-void print6(int n)
-{
+
+void print6(int n){
      for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n-i+1;j++)
@@ -62,10 +62,55 @@ void print6(int n)
         }
         cout<<endl;
     }
-
-
+}
+void print7(int n){
+     for(int i=0;i<n;i++){   
+          //space
+          for(int j=0;j<n-i-1;j++)
+            {
+            cout<<" ";
+            } 
+          //star
+          for(int j=0;j<2*i+1;j++)
+           {
+            cout<<"*";
+            }
+           //space
+          for(int j=0;j<n-i-1;j++){
+             cout<<" ";
+          }
+          cout<<endl;
+        }
+}
+void print8(int n){
+   for(int i=0;i<n;i++){   
+          //space
+          for(int j=0;j<i;j++) {
+            cout<<" ";}
+            
+          //star
+          for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+            }
+           //space
+          for(int j=0;j<i;j++){
+             cout<<" ";
+          }
+          cout<<endl;
+    } 
 }
 
+void print9(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars = i;
+        if(i>n) stars = 2*n -i;
+        for(int j=1;j<=stars;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+  
 
 int main()
 {  
@@ -75,7 +120,15 @@ int main()
     {
         int n;
         cin>>n;
-        print6(n);
+        // print1(n);
+        // print2(n);
+        // print3(n);
+        // print4(n);
+        // print5(n);
+        // print6(n);
+        // print7(n);
+        // print8(n);
+         print9(n);
     }
     
 
