557d55a0c19db4c7d489507f62b4ceaee5431c7e
gprs1022 <pradeep1022gprs@gmail.com>
2025-04-23 08:03:59 +0530

    Day one

---
+++ April-2025/addition.cpp
@@ -0,0 +1,19 @@
+// C program to add two numbers
+#include <stdio.h>
+
+int main() {
+    int a, b, sum = 0;
+  
+  	// Read two numbers from the user
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    
+    // Calculate the addition of a and b
+    // using '+' operator
+    sum = a + b;
+
+    printf("Sum: %d", sum);
+
+    return 0;
+}
---
+++ April-2025/subtraction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b,c;
+    cout<<"Enter the value of a and b";
+    cin>>a>>b;
+    c=a-b;
+    cout<<"the subtraction of a and b is"<<c;
+    return 0;
+}
