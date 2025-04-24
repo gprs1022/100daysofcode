9ec5315c5e610b18537dec2be8ea29bc45eaa5dd
gprs1022 <pradeep1022gprs@gmail.com>
2025-04-24 11:26:28 +0530

    sizeof datatypes

---
+++ April-2025/sizeof.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Size of int is: " << sizeof(int) << " bytes\n";
+    cout << "Size of char is: " << sizeof(char) << " bytes\n";
+    cout << "Size of float is: " << sizeof(float) << " bytes\n";
+    cout << "Size of double is: " << sizeof(double) << " bytes\n";
+    cout << "Size of long is: " << sizeof(long) << " bytes\n";
+    cout << "Size of long long is: " << sizeof(long long) << " bytes\n";
+    cout << "Size of short is: " << sizeof(short) << " bytes\n";
+    cout << "Size of long double is: " << sizeof(long double) << " bytes\n";
+    cout << "Size of bool is: " << sizeof(bool) << " bytes\n";
+    cout << "Size of wchar_t is: " << sizeof(wchar_t) << " bytes\n";
+    cout << "Size of string is: " << sizeof(string) << " bytes\n";
+    cout << "Size of vector is: " << sizeof(vector<int>) << " bytes\n";
+    cout << "Size of map is: " << sizeof(map<int, int>) << " bytes\n";
+    cout << "Size of set is: " << sizeof(set<int>) << " bytes\n";
+    cout << "Size of unordered_map is: " << sizeof(unordered_map<int, int>) << " bytes\n";
+    cout << "Size of unordered_set is: " << sizeof(unordered_set<int>) << " bytes\n";
+
+    return 0;
+}
