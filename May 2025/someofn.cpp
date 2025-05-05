7c98a612cf3b29f64bc8885ac2a62c362afd897b
gprs1022 <pradeep1022gprs@gmail.com>
2025-05-05 10:55:06 +0530

    5

---
+++ May 2025/someofn.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter the value of n:";
+  cin>>n;
+  int sumofNaturalNumber= n*(n+1)/2;
+
+  cout<<"Sum of natural number is:"<<sumofNaturalNumber<<endl;
+  return 0;
+  
+}
